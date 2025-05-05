8aa72c412e386d07e96126b5d455b5ed63504049
Erik Wahlström <ew@kth.se>
2025-05-05 12:33:50 +0200

    flip och baksida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// This file is intentionally left blank.
+// filepath: [script.js](http://_vscodecontentref_/0)
+document.querySelectorAll('.player-card').forEach(card => {
+    card.addEventListener('click', () => {
+      card.querySelector('.card-inner').classList.toggle('is-flipped');
+    });
+  });
